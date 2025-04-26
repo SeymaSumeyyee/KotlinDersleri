eadfc96c372e5cb471ffec3a2f3543dc9b7be23e
SeymaSumeyyee <167340335+SeymaSumeyyee@users.noreply.github.com>
2025-04-26 22:17:53 +0300

    Odev1
    Degiskenler

---
+++ hom1.kt
@@ -0,0 +1,57 @@
+package com.example.kotlindersleri.degiskenler
+
+fun main() {
+    val city="Ankara"
+    val country="Türkiye"
+    val phoneNo="052358545"
+    val postaKodu="12356"
+    val email="seyma@gmail.com"
+    val meslek="Mühendis"
+    var stok=52
+    val customerName="Şeyma"
+    var bakiye=1000
+    val birthday="22.08.2002"
+    var salary=80000
+    var medeniDurum="bekar"
+    val urunYorum="iyi"
+    val odemeTarihi="07.01.2025"
+    val odeme=true
+    val siparisAdet=20
+    val arabaModel=2020
+    val kitapAdi="Efsuncu Baba"
+    val yayınTarih="20.01.2000"
+    val indirimMiktarı=20
+    val odaSayısı=3
+    val enlem=23
+    val boylam=45
+    val urunAdi="kitap"
+    val yemekFiyat=250
+    val marka="Acer"
+    val muzikAdi="Follow You"
+    val videoSure=20.53
+    val urunPuan=4.5
+    val resimAd="okul"
+    val dosyaFormat=".docx"
+    val renk="mor"
+    val renkKodu=100
+    val telefonModel="S23"
+    val ekranBoyut=10
+    val agirlik=56
+    val ulusalGun="23 Nisan"
+    val tatilGun="Pazar"
+    val rezervasyonTarih="12.07.2025"
+    val sokakAd="Çiçek"
+    val otobusHat="500T"
+    val kalanDk=25
+    val takipKod=2310
+    val kuponSuresi=5
+    val kuponKodu=1234
+    val faturaAdresi="Çiçek Sokak No:4"
+
+    println("Sehir: $city")
+    println("Ulke: $country")
+    println("Telefon Modeli: $telefonModel")
+    println("Kitap Adi: $kitapAdi")
+    println("Tatil Gunu: $tatilGun")
+
+}
